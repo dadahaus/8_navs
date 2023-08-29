ad6d51d3aef8eddab643fa9d27c1681238b4a7a4
kosme <casadadaista@gmail>
2023-08-29 20:42:03 +0200

    8_navs with pnpm

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	// import Nav from './components/Nav.svelte';
+</script>
+
+<svelte:head>
+	<title>first navbar</title>
+	<meta name="description" content="portfolio" />
+</svelte:head>
+
+	<section>
+
+		<h1>
+			hello
+		</h1>
+	</section>
+
+<style>
+
+</style>
